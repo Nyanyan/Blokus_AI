7c25ca87a7df3e1afec575cd195d3916a009af12
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2025-12-31 11:08:47 +0900

    struct Mino

--- src/engine.cpp
+++ src/engine.cpp
@@ -6,6 +6,13 @@ constexpr int FIL = 1; // filled
 constexpr int CNR = 2; // corner
 constexpr int EDG = 3; // edge
 
+
+struct Mino {
+    std::vector<std::vector<int>> mino;
+    size_t h;
+    size_t w;
+};
+
 const std::vector<std::vector<int>> M1 = {
     {CNR, EDG, CNR},
     {EDG, FIL, EDG},
@@ -157,18 +164,19 @@ const std::vector<std::vector<int>> M5Z = {
 };
 
 
-const std::vector<std::vector<std::vector<int>>> unique_minos = {
-    M1, 
-    M2, 
-    M3V, M3I, 
-    M4I, M4L, M4N, M4P, M4T,
-    M5F, M5I, M5L, M5N, M5P, M5T, M5U, M5V, M5W, M5X, M5Y, M5Z
+const std::vector<Mino> unique_minos = {
+    {M1, M1.size(), M1[0].size()},
+    {M2, M2.size(), M2[0].size()},
+    {M3V, M3V.size(), M3V[0].size()}, {M3I, M3I.size(), M3I[0].size()},
+    {M4I, M4I.size(), M4I[0].size()}, {M4L, M4L.size(), M4L[0].size()}, {M4N, M4N.size(), M4N[0].size()}, {M4P, M4P.size(), M4P[0].size()}, {M4T, M4T.size(), M4T[0].size()},
+    {M5F, M5F.size(), M5F[0].size()}, {M5I, M5I.size(), M5I[0].size()}, {M5L, M5L.size(), M5L[0].size()}, {M5N, M5N.size(), M5N[0].size()}, {M5P, M5P.size(), M5P[0].size()}, {M5T, M5T.size(), M5T[0].size()},
+    {M5U, M5U.size(), M5U[0].size()}, {M5V, M5V.size(), M5V[0].size()}, {M5W, M5W.size(), M5W[0].size()}, {M5X, M5X.size(), M5X[0].size()}, {M5Y, M5Y.size(), M5Y[0].size()}, {M5Z, M5Z.size(), M5Z[0].size()}
 };
 
 
 
-void print_mino(const std::vector<std::vector<int>>& mino) {
-    for (const auto& row : mino) {
+void print_mino(const Mino& mino) {
+    for (const auto& row : mino.mino) {
         for (const auto& cell : row) {
             if (cell == DNC) {
                 std::cout << " ";
